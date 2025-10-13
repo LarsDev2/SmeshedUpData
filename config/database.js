daccad54003e1a4ed338760b0bc54efbe7f75ba2
Lars Pieters <lars.pieters@student.howest.be>
2025-10-13 15:53:08 +0200

    changed database.js

--- config/database.js
+++ config/database.js
@@ -17,7 +17,8 @@ module.exports = ({ env }) => {
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+          rejectUnauthorized: false, // <-- allows self-signed certs
+
         },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
