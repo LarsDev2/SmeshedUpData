57007a617a217f0773a738d88da547c3c4df4c61
Lars Pieters <lars.pieters@student.howest.be>
2025-10-14 22:15:54 +0200

    changed server.js with aggregateError console.logs

--- config/server.js
+++ config/server.js
@@ -1,7 +1,17 @@
+process.on('unhandledRejection', (err) => {
+  console.error('🔴 Unhandled Rejection:', err);
+});
+process.on('uncaughtException', (err) => {
+  console.error('🔴 Uncaught Exception:', err);
+});
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
