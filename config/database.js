530faef48597df4ba08c5464e8d5319b3ed8144f
Lars Pieters <lars.pieters@student.howest.be>
2025-10-14 23:01:24 +0200

    changed database.js 1.8

--- config/database.js
+++ config/database.js
@@ -1,17 +1,23 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false, // required for Render
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'postgres');
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        ssl: env.bool('DATABASE_SSL', false)
+          ? { rejectUnauthorized: false }
+          : false,
       },
-      schema: env('DATABASE_SCHEMA', 'public'),
+      pool: { min: 2, max: 10 },
     },
-    pool: {
-      min: env.int('DATABASE_POOL_MIN', 2),
-      max: env.int('DATABASE_POOL_MAX', 10),
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: 60000,
     },
-    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-  },
-});
+  };
+};
