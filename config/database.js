f2087ce8582c31cf1b1de9ac3f5bfa0994450475
Lars Pieters <lars.pieters@student.howest.be>
2025-10-14 18:59:24 +0200

    database.js changed 1.2

--- config/database.js
+++ config/database.js
@@ -11,9 +11,7 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false)
-          ? { rejectUnauthorized: false }
-          : false,
+        ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
